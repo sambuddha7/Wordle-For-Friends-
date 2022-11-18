7544ae52d123a03b165b5a013e289faf0de2310e
sambuddha7 <sbiswas7@illinois.edu>
2022-11-18 13:17:59 -0600

    Add files via upload

--- play_wordle.py
+++ play_wordle.py
@@ -1,21 +1,46 @@
 from wordle import Wordle
-
+import random
+import getpass
 def main():
     print("Wordle For Friends!\n")
     print("Pass a secret 5 letter word for your friend to guess!\n")
-    sec = input("Type Secret Word:")
+    print("Or type 'R' for a random word\n")
+    data_src = "dataset/five_letter_words.txt"
+    data_set = data_to_sets(data_src)
+    
+    #sec = input("Type Secret Word:").upper()
+    sec = getpass.getpass(prompt = "Type Secret Word:").upper()
+    if sec == 'R':
+      sec = random.choice(list(data_set))
+    while len(sec) != 5 or sec not in data_set or sec == 'R':
+      if sec == 'R':
+        sec = random.choice(list(data_set))
+      sec = getpass.getpass(prompt = "Word must be a valid 5 letter english word:").upper()
+    
     wordle = Wordle(sec)
     while wordle.can_attempt():
         inp = input("Enter guess:")
         if len(inp) != 5:
           print("Enter input of correct length")
           continue
+        if inp.upper() not in data_set:
+          print("Not in word list\n")
+          continue
         wordle.attempt(inp)
-        wordle.guesses(inp)
+        wordle.draw_box(wordle.guesses(inp))
+        
     if wordle.is_solved():
       print("You guessed the word!")
-    else: print("Attempts over! Better luck next time:(") 
+    else: 
+      print("Attempts over! Better luck next time:(") 
+      print(f"The word was: {wordle.secret}")
 
-
+def data_to_sets(path:str):
+  word_set = set()
+  with open(path, "r") as f:
+    for line in f.readlines():
+      word = line.strip().upper()
+      word_set.add(word)
+  return word_set
 if __name__ == "__main__":
     main()
--- wordle.py
+++ wordle.py
@@ -6,7 +6,14 @@ class Wordle:
     self.words_guessed = []
     self.colored_guesses = []
     self.attempts_left = 6
+    self.dictionary = {}
+    for c in self.secret:
+      if c in self.dictionary:
+        self.dictionary[c] += 1
+      else:
+        self.dictionary[c] = 1
     pass
+    
   def is_solved(self):
     if len(self.words_guessed) > 0 and self.words_guessed[-1] == self.secret:
       return True
@@ -24,10 +31,18 @@ class Wordle:
   def guess(self, word: str):
     result = []
     word = word.upper()
+    guess_dict = {}
+    #dictionary to compare occurences of letter with the secret word
+    for i in range(5):
+      if word[i] in guess_dict:
+        guess_dict[word[i]] += 1
+      else:
+        guess_dict[word[i]] = 1
     for i in range(5):
       letter = Letter(word[i])
       letter.position = word[i] == self.secret[i]
-      letter.contains = word[i] in self.secret
+      if word[i] in self.secret and guess_dict[word[i]] == self.dictionary[word[i]]:
+        letter.contains = True
       result.append(letter)
     return result
   #traverses through the word and prints out colored letters
@@ -43,9 +58,24 @@ class Wordle:
         color = Fore.WHITE
       colored_letter = color + l.char + Fore.RESET
       result_with_color.append(colored_letter)
-      joined = "".join(result_with_color)
+      joined = " ".join(result_with_color)
     self.colored_guesses.append(joined)
+    return self.colored_guesses
+    # for c in self.colored_guesses: #printing the colored letters
+    #   print(c)
+    # for i in range(self.attempts_left): #printing the blank spaces
+    #   print(('_ ' * 5) + "\n")
+  def draw_box(self, colored_guesses):
+    char = 9 #5 letters + 4 spaces
+    padding = 2
+    top_border = '┌' + ('─' * char) + '┐'
+    print(top_border)
     for c in self.colored_guesses:
-      print(c)
-    for i in range(self.attempts_left):
-      print(('_' * 5) + "\n")
+      middle = '│' + c + '│'
+      print(middle)
+    for i in range(self.attempts_left): #printing the blank spaces
+      middle =  '│' + ('_ ' * 4) + '_│'
+      print(middle)
+    bottom_border = '└' + ('─' * char) + '┘'
+    print(bottom_border)
+    
